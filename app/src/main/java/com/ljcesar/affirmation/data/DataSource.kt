305a59dc09070096d101f86b440f557a91f03405
Jules-César LUSANGA <22li214@esisalama.org>
2025-01-29 12:42:35 +0200

    feat : added topbar, expanded list item

--- app/src/main/java/com/ljcesar/affirmation/data/DataSource.kt
+++ app/src/main/java/com/ljcesar/affirmation/data/DataSource.kt
@@ -6,17 +6,15 @@ import com.ljcesar.affirmation.model.Country
 class DataSource {
      fun loadCountries(): List<Country> {
          return listOf<Country>(
-             Country(R.string.country1, R.string.capital1, R.string.code1, R.drawable.image1),
-             Country(R.string.country2, R.string.capital2, R.string.code2, R.drawable.image2),
-             Country(R.string.country3, R.string.capital3, R.string.code3, R.drawable.image3),
-             Country(R.string.country4, R.string.capital4, R.string.code4, R.drawable.image4),
-             Country(R.string.country5, R.string.capital5, R.string.code5, R.drawable.image5),
-             Country(R.string.country6, R.string.capital6, R.string.code6, R.drawable.image6),
-             Country(R.string.country7, R.string.capital7, R.string.code7, R.drawable.image7),
-             Country(R.string.country8, R.string.capital8, R.string.code8, R.drawable.image8),
-             Country(R.string.country9, R.string.capital9, R.string.code9, R.drawable.image9),
-             Country(R.string.country10, R.string.capital10, R.string.code10, R.drawable.image10),
-
-             )
+             Country(R.string.country1, R.string.capital1, R.string.code1, R.string.description1, R.drawable.image1),
+             Country(R.string.country2, R.string.capital2, R.string.code2, R.string.description2, R.drawable.image2),
+             Country(R.string.country3, R.string.capital3, R.string.code3, R.string.description3, R.drawable.image3),
+             Country(R.string.country4, R.string.capital4, R.string.code4, R.string.description4, R.drawable.image4),
+             Country(R.string.country5, R.string.capital5, R.string.code5, R.string.description5, R.drawable.image5),
+             Country(R.string.country6, R.string.capital6, R.string.code6, R.string.description6, R.drawable.image6),
+             Country(R.string.country7, R.string.capital7, R.string.code7, R.string.description7, R.drawable.image7),
+             Country(R.string.country8, R.string.capital8, R.string.code8, R.string.description8, R.drawable.image8),
+             Country(R.string.country9, R.string.capital9, R.string.code9, R.string.description9, R.drawable.image9),
+             Country(R.string.country10, R.string.capital10, R.string.code10, R.string.description10, R.drawable.image10),)
      }
 }
--- app/src/main/java/com/ljcesar/affirmation/model/Country.kt
+++ app/src/main/java/com/ljcesar/affirmation/model/Country.kt
@@ -7,5 +7,6 @@ data class Country(
     @StringRes val stringNameResourceId: Int,
     @StringRes val stringCapitalResourceId: Int,
     @StringRes val stringCodeResourceId: Int,
+    @StringRes val stringDescriptionResourceId: Int,
     @DrawableRes val imageResourceId: Int
 )
